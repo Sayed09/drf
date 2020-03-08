952b90279a946de5482675d15499e85a9a675f97
sayed09 <sayed.hassan@me-solshare.com>
2020-03-08 18:02:31 +0600

    Add python package for API documentation

--- django_everything/urls.py
+++ django_everything/urls.py
@@ -17,9 +17,25 @@ from django.urls import include, path
 from django.contrib import admin
 from rest_framework.documentation import include_docs_urls
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Django Everything",
+      default_version='v1',
+      description="",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/', include("api.urls")),
-    path('docs/', include_docs_urls(title='Hello world'))
+    path('doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
